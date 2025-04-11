e5991bc85b6bb888d95b7293a06757a60333ca85
lishu-rumsan <lishu.maharjan@rumsan.net>
2025-04-11 20:38:19 +0545

    feat: swagger-setup

--- flare/src/main.ts
+++ flare/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Curate AI')
+    .setDescription('The Curate AI API description')
+    .addTag('Curate AI')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
+  const port = process.env.PORT || 3003;
+  await app.listen(port);
+
+  Logger.log(`Application is running on: http://localhost:${port}`);
+  Logger.log(`Swagger UI: http://localhost:${port}/swagger`);
 }
 bootstrap();
