f5be46808f7e37e9f198ada358ccb65aa9997f81
lishu-rumsan <lishu.maharjan@rumsan.net>
2025-04-23 21:57:31 +0545

    feat: cors-updated

--- flare/src/main.ts
+++ flare/src/main.ts
@@ -6,6 +6,8 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Curate AI')
     .setDescription('The Curate AI API description')
